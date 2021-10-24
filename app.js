628d2fb82d1f880cae2cc021ee395c01ac0e31a6
LaluPrasad M <laluprasad.73515274@gmail.com>
2021-10-24 23:12:44 +0530

    handled cors errors

--- app.js
+++ app.js
@@ -10,7 +10,24 @@ const products = require("./api/routes/products");
 const carts = require("./api/routes/carts");
 
 const app = express();
-app.use(bodyparser.urlencoded({extended:false}));
+
+//Preventing CORS errors
+//to Run Client and Server on different Systems
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested_With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.use("/users", users);
@@ -20,5 +37,20 @@ app.use("/categories", categories);
 app.use("/tags", tags);
 app.use("/products", products);
 app.use("/carts", carts);
+
+//Handling Errors
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
