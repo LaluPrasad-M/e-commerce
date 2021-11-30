5e14abcb46c037bcd9eb460d8a505d7d37fa89b0
LaluPrasad M <laluprasad.73515274@gmail.com>
2021-11-30 18:27:57 +0530

    new changes

--- data/custom_data/mappings/custom_mapping.js
+++ data/custom_data/mappings/custom_mapping.js
@@ -1,11 +1,11 @@
 const role_modules_mapping = {
   //Root Users
-  "RootAdmin_g9YUS2mRBc": ["module1A_9nhxVngR9U","submoduleXYZ_Ac1WNvaICb","emenucard_Mf9eyDrYZL","submoduleXYZ1_khHGFj5Bj1"],
-  "RootManager_lwvOH8Qvqy": ["module1A_9nhxVngR9U","submoduleXYZ_Ac1WNvaICb","emenucard_Mf9eyDrYZL"],
+  "RootAdmin_g9YUS2mRBc": ["module1A_9nhxVngR9U", "submoduleXYZ_Ac1WNvaICb", "emenucard_Mf9eyDrYZL", "submoduleXYZ1_khHGFj5Bj1"],
+  "RootManager_lwvOH8Qvqy": ["module1A_9nhxVngR9U", "submoduleXYZ_Ac1WNvaICb", "emenucard_Mf9eyDrYZL"],
   //Restaurant users
   "ShopManager_uao8RNiWUy": ["emenucard_Mf9eyDrYZL"],
   //Restaurant Customers
-  "ShopABCCustomer_a6aYeCKPj6":["emenucard_Mf9eyDrYZL","submoduleXYZ1_khHGFj5Bj1"],
+  "ShopABCCustomer_a6aYeCKPj6": ["emenucard_Mf9eyDrYZL", "submoduleXYZ1_khHGFj5Bj1"],
 };
 
 const module_content_mapping = {
